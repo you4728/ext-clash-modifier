16175427bc3d33982eece2186d034e18336e3ba1
you4728 <107193173+you4728@users.noreply.github.com>
2024-03-22 11:00:34 +0800

    Update template.js

--- src/template.js
+++ src/template.js
@@ -45,7 +45,6 @@ proxy-groups:
     interval: 300
     strategy: consistent-hashing
     proxies:
-      - DIRECT
       - _PROXY_NAME
   - name: ⚖️ 负载均衡-轮询
     type: load-balance
@@ -53,7 +52,6 @@ proxy-groups:
     interval: 300
     strategy: round-robin
     proxies:
-      - DIRECT
       - _PROXY_NAME
 
 rules:
